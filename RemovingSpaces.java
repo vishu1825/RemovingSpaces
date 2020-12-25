649f420af7af32f84cc127cf35cfacb56a85fc05
Vishwajeet Samal <vishwajeetsaml1994@gmail.com>
2020-12-25 19:03:50 +0530

    Added removing Spaces Program

---
+++ RemovingSpaces.java
@@ -0,0 +1,9 @@
+class RemovingSpaces
+ {
+  public static void main(String[] args)
+    {
+  String str= "My Name is Vishwajeet";//using direct method
+    str=str.replaceAll(" ","");
+   System.out.println(str);
+    }
+ }
